b6fe77e6af531c3f14c9471b59989a74d01d832d
Jake Marwil <rjm02006@gmail.com>
2015-02-09 11:16:03 -0700

    add controllers and include CRUD methods

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,46 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to events_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update_attributes(event_params)
+    redirect_to events_path
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:date, :description, :location_id, :requires_id)
+  end
+
+end
---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,47 @@
+class LocationsController < ApplicationController
+
+    def index
+      @locations = Location.all
+    end
+
+    def show
+      @location = Location.find(params[:id])
+      @events = @location.events
+    end
+
+    def new
+      @location = Location.new
+    end
+
+    def create
+      @location = Location.new(location_params)
+      if @location.save
+        redirect_to locations_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @location = Location.find(params[:id])
+    end
+
+    def update
+      @location = Location.find(params[:id])
+      @location.update_attributes(location_params)
+      redirect_to locations_path
+    end
+
+    def destroy
+      @locations = Location.find(params[:id])
+      @location.destroy
+      redirect_to locations_path
+    end
+
+    private
+
+    def location_params
+      params.require(:location).permit(:name, :address, :location_id, :zipcode)
+    end
+
+end
