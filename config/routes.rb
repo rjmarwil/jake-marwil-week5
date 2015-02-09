debab8971e03620aca70129e32db1d444c44b023
Jake Marwil <rjm02006@gmail.com>
2015-02-09 11:40:31 -0700

    format code to be easier to read

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'locations#index'
-   resources :locations do
-     resources :events
-   end
+  resources :locations do
+    resources :events
+  end
 
    resources :events
 
